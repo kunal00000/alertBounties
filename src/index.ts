fe199e0804376f573f16bae2953fd53a8b0e753e
kunal00000 <kunalverma2468@gmail.com>
2023-10-11 10:26:24 +0530

    setup for render

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,51 @@
-import fs from "fs";
-import path from "path";
-import puppeteer from "puppeteer";
-import dotenv from "dotenv";
-import { Resend } from "resend";
-import { Twilio } from "twilio";
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+import { Resend } from 'resend';
 dotenv.config();
+
+const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World!');
+});
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-
-const client = new Twilio(accountSid, authToken);
-
 const main = async () => {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
 
-  await page.goto("https://console.algora.io/");
+  await page.goto('https://console.algora.io/');
 
   await page.setCookie({
-    name: "__Secure-next-auth.session-token",
-    value: "a8c7f684-66c9-4406-95cc-800a564ab263",
-    path: "/"
+    name: '__Secure-next-auth.session-token',
+    value: 'a8c7f684-66c9-4406-95cc-800a564ab263',
+    path: '/'
   });
 
-  await page.waitForSelector("td > div > a > .gap-2");
+  await page.waitForSelector('td > div > a > .gap-2');
   await page.evaluate(() => {
     window.scrollTo(0, 10000);
   });
   await page.waitForNetworkIdle({ idleTime: 2500 });
 
   const latestIssue = await page.$eval(
-    "td > div > a > div > .font-medium",
+    'td > div > a > div > .font-medium',
     (element) => element.textContent
   );
   const latestDesc = await page.$eval(
-    "td > div > a > .gap-2",
+    'td > div > a > .gap-2',
     (element) => element.textContent
   );
-  const latestLink = await page.$eval("td > div > .w-full", (element) =>
-    element.getAttribute("href")
+  const latestLink = await page.$eval('td > div > .w-full', (element) =>
+    element.getAttribute('href')
   );
 
   const lagBounty = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "../src/data.json"), "utf8")
+    fs.readFileSync(path.join(__dirname, '../src/data.json'), 'utf8')
   );
 
   if (
@@ -58,48 +59,48 @@ const main = async () => {
     };
 
     fs.writeFileSync(
-      path.join(__dirname, "../src/data.json"),
+      path.join(__dirname, '../src/data.json'),
       JSON.stringify(latestBounty)
     );
 
     const sIssues = await page.$$eval(
-      "td > div > a > div > .font-medium",
+      'td > div > a > div > .font-medium',
       (elements) => Array.from(elements).map((element) => element.textContent)
     );
 
-    const sDesc = await page.$$eval("td > div > a > .gap-2", (elements) =>
+    const sDesc = await page.$$eval('td > div > a > .gap-2', (elements) =>
       Array.from(elements).map((element) => element.textContent)
     );
 
-    const sLinks = await page.$$eval("td > div > .w-full", (elements) =>
-      Array.from(elements).map((element) => element.getAttribute("href"))
+    const sLinks = await page.$$eval('td > div > .w-full', (elements) =>
+      Array.from(elements).map((element) => element.getAttribute('href'))
     );
 
     const idxTillLastUpdate = sIssues.findIndex((l) => l == lagBounty.issue);
 
     if (idxTillLastUpdate === -1) {
-      console.log("Some error occurred");
+      console.log('Some error occurred');
     } else {
       const newIssues = sIssues.slice(0, idxTillLastUpdate);
       const newDesc = sDesc.slice(0, idxTillLastUpdate);
       const newLinks = sLinks.slice(0, idxTillLastUpdate);
 
-      console.log("Sending notification for new issues");
+      console.log('Sending notification for new issues');
 
-      let htmlmes = "<h1>New issues found</h1><br><ol>";
+      let htmlmes = '<h1>New issues found</h1><br><ol>';
       for (let i = 0; i < newIssues.length; i++) {
         htmlmes += `<li><a href="${newLinks[i]}">${newIssues[i]}</a> > ${newDesc[i]}</li>`;
       }
       htmlmes += `</ol>`;
       // send email notification for new issues
       resend.emails.send({
-        from: "onboarding@resend.dev",
-        to: "kunalverma2468@gmail.com",
-        subject: "New Issues Found",
+        from: 'onboarding@resend.dev',
+        to: 'kunalverma2468@gmail.com',
+        subject: 'New Issues Found',
         html: `${htmlmes}`
       });
 
-      let message = "New issues found\n\n";
+      let message = 'New issues found\n\n';
       for (let i = 0; i < newIssues.length; i++) {
         message += `${newIssues[i]} > ${newDesc[i]}\n${newLinks[i]}\n\n`;
       }
@@ -119,12 +120,22 @@ const main = async () => {
       //   .catch((err) => console.log(err));
     }
   } else {
-    console.log("No new issues found");
+    console.log('No new issues found');
   }
 
   await browser.close();
 };
 
-main();
+// main();
 
-// setInterval(main, 60000 * 5);
+setInterval(main, 60000 * 4);
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+// import { Twilio } from "twilio";
+
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = new Twilio(accountSid, authToken);
